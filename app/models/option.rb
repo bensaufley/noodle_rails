96b99b54cfc4f8b0c9dae9c49af802d8f96e40c2
Ben Saufley <subscriptions@bensaufley.com>
2016-05-04 15:59:09 -0400

    Ensure consistent vote ordering

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,4 +1,6 @@
 class Option < ActiveRecord::Base
   belongs_to :noodle
   has_many :votes, dependent: :destroy
+
+  default_scope { order(created_at: :asc) }
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,5 @@ class Vote < ActiveRecord::Base
   belongs_to :vote_set
   belongs_to :option
 
-  default_scope { includes(:vote_set).order('vote_sets.created_at ASC') }
+  default_scope { includes(:vote_set, :option).order('vote_sets.created_at ASC, options.created_at ASC') }
 end
